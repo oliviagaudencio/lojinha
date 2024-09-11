215837e1fd42422c48cd37e12ba8b41c6a0489fa
oliviagaudencio <pintogaudencioolivia@gmail.com>
2024-09-11 15:24:34 -0300

    primeirinho

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ from flask import Flask, render_template, request, flash, redirect
 app = Flask(__name__)
 from database import db
 from flask_migrate import Migrate
-from models import Reserve
-app.config['SECRET_KEY'] = ['2c1685c157fe2450fda443a0753bb69f']
+from models import Vendas
+app.config['SECRET_KEY'] = ['ch3Gu743RaM4c0nh4C0mun1sm0']
 
 # drive://reserve:senha@servidor/banco_de_dados
-conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/crud_flask"
+conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/vendas"
 app.config['SQLALCHEMY_DATABASE_URI'] = conexao
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
@@ -27,71 +27,66 @@ migrate = Migrate(app, db)
 def index():
     return render_template('index.html')
 
-@app.route('/dados', methods=['POST'])
-def dados():
-    flash('Dados enviados!')
-    dados = request.form
-    return render_template('dados.html', dados=dados)
-
-@app.route('/reserve')
-def reserve():
-    u = Reserve.query.all()
+@app.route('/vendas')
+def vendas():
+    u = Vendas.query.all()
     print(u)
-    return render_template('reserve_lista.html', dados=u)
+    return render_template('vendas_lista.html', dados=u)
 
-@app.route('/reserve/add')
+@app.route('/vendas/add')
 def reserve_add():
-    return render_template("reserve_add.html")
+    return render_template("vendas_add.html")
 
-@app.route('/reserve/save', methods=['POST'])
+@app.route('/vendas/save', methods=['POST'])
 def reserve_save():
     data = request.form.get('data')
-    cliente = request.form.get('cliente')
-    servico = request.form.get('servico')
-    if data and cliente and servico:
-        reserve = Reserve(data, cliente, servico)
-        db.session.add(reserve)
+    valor = request.form.get('valor')
+    vendedor = request.form.get('vendedor')
+    if data and valor and vendedor:
+        vendas = Vendas(data, valor, vendedor)
+        db.session.add(vendas)
         db.session.commit()
-        flash('Usuário cadastrado com sucesso!!!')
-        return redirect('/reserve')
+        flash('Venda cadastrada')
+        return redirect('/vendas')
     else:
         flash('Preencha todos os campos')
-        return redirect('/reserve/add')
+        return redirect('/vendas/add')
 
-@app.route('/reserve/remove/<int:id>')
-def reserve_remove(id):
+@app.route('/vendas/remove/<int:id>')
+def vendas_remove(id):
     if id > 0:
-        reserve = Reserve.query.get(id)
-        db.session.delete(reserve)
+        vendas = Vendas.query.get(id)
+        db.session.delete(vendas)
         db.session.commit()
-        flash('Reserva removida com sucesso!!!')
-        return redirect('/reserve')
+        flash('Venda removida!')
+        return redirect('/vendas')
     else:
-        flash('Caminho Incorreto!')
-        return redirect('/reserve')
+        flash('Incorreto')
+        return redirect('/vendas')
     
-@app.route('/reserve/edita/<int:id>')
-def reserve_edita(id):
-    reserve = Reserve.query.get(id)
-    return render_template("reserve_edita.html", dados = reserve)
+@app.route('/vendas/editar/<int:id>')
+def vendas_editar(id):
+    vendas = Vendas.query.get(id)
+    return render_template("vendas_editar.html", dados = vendas)
 
-@app.route('/reserve/edita/save', methods=['POST'])
-def reserve_edita_save():
+@app.route('/vendas/editar/save', methods=['POST'])
+def vendas_editar_save():
     data = request.form.get('data')
-    cliente = request.form.get('cliente')
-    servico = request.form.get('servico')
+    valor = request.form.get('valor')
+    vendedor = request.form.get('vendedor')
     id = request.form.get('id')
-    if id and data and cliente and servico:
-        reserve = Reserve.query.get(id)
-        reserve.data = data
-        reserve.cliente = cliente
-        reserve.servico = servico
+    if id and data and valor and vendedor:
+        vendas = Vendas.query.get(id)
+        vendas.data = data
+        vendas.valor = valor
+        vendas.vendedor = vendedor
         db.session.commit()
-        flash('Dados atualizados com sucesso!')
-        return redirect('/reserve')
+        flash('Venda atualizada com sucesso!')
+        return redirect('/vendas')
     else:
-        flash('Faltando dados!!')
-        return redirect('/reserve')
+        flash('Complete todos os dados!!')
+        return redirect('/vendas')
 
 if __name__ == '__main__':
     app.run()
+
--- models.py
+++ models.py
@@ -1,16 +1,16 @@
 from database import db
 
-class Reserve(db.Model):
-    __tablename__ = "reserve"
+class Vendas(db.Model):
+    __tablename__ = "vendas"
     id = db.Column(db.Integer, primary_key = True)
     data = db.Column(db.Date)
-    cliente = db.Column(db.String(100))
-    servico = db.Column(db.String(100))
+    valor = db.Column(db.Float(10,2))
+    vendedor = db.Column(db.String(100))
 
-    def __init__(self, data, cliente, servico):
+    def __init__(self, data, valor, vendedor):
         self.data = data
-        self.cliente = cliente
-        self.servico = servico
+        self.valor = valor
+        self.vendedor = vendedor
 
     def __repr__(self):
-        return "<Reserva {}>".format(self.cliente)
+        return "<Vendedor {}>".format(self.vendedor)
